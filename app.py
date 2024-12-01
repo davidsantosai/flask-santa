804e1322d474524c1c3d85fcd1cbad4bc843d208
David <80417730+davidsantosai@users.noreply.github.com>
2024-12-01 17:17:50 -0500

    Implement sample full stack app

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)  # _name_ takes the name of the parent folder
 
@@ -6,14 +6,25 @@ app = Flask(__name__)  # _name_ takes the name of the parent folder
 @app.route(
     "/"
 )  # app is a decorator used to store recursive data - GET Method by default
-def hello_world():
+def home():
     """
         Returns 'Hello, World!' in response to a GET request to the root URL.
     gi
         This function serves as the entry point for the Flask application and
         demonstrates the basic routing capabilities of the framework.
     """
-    return "Hello, World!"
+    return render_template("home.html", my_name="santa")
+
+
+@app.route("/about")
+def about():
+    """
+    Returns the content of the 'about.html' template in response to a GET request to the '/about' URL.
+
+    This function demonstrates how to render a template using Flask's `render_template` function.
+    The 'about.html' template is located in the 'templates' directory of the application.
+    """
+    return render_template("about.html", about_text="lorem ipsum")
 
 
 if __name__ == "__main__":
